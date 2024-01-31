1fa8bed95e6bb5ba5d8082babc9af4c759948136
s24 <127821544+S24seguridadelectronica@users.noreply.github.com>
2024-01-31 09:53:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-const container = document.querySelector('.container');
-const imageWrapper = document.querySelector('.image-wrapper');
-const image = document.querySelector('.image-wrapper img');
+window.onload = function() {
+    window.scrollTo(0, 0);
+  };
 
-let isDragging = false;
-let startPosition = 0;
-let currentTranslate = 0;
-let prevTranslate = 0;
-let animationId = 0;
-
-function dragStart(e) {
-    isDragging = true;
-    startPosition = e.clientX;
-    prevTranslate = currentTranslate;
-    cancelAnimationFrame(animationId);
-}
-
-function dragEnd() {
-    isDragging = false;
-}
-
-function drag(e) {
-    if (isDragging) {
-        const currentPosition = e.clientX;
-        currentTranslate = prevTranslate + currentPosition - startPosition;
-        image.style.transform = `translateX(${currentTranslate}px)`;
-    }
-}
-
-container.addEventListener('mousedown', dragStart);
-container.addEventListener('mouseup', dragEnd);
-container.addEventListener('mousemove', drag);
+  
